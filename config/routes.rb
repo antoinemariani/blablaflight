1e30a1cb58347e4ff439d97ae339b0a28bf9b8d9
Nicolas Fornaro <nicolasfornaro75@gmail.com>
2022-11-21 17:35:36 +0100

    routes ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :flights do
+    resources :bookings, only: %I[index show new create]
+  end
+  resources :bookings, only: %I[edit update destroy]
   # Defines the root path route ("/")
   # root "articles#index"
 end
