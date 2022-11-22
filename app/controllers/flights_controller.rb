1f90892287e5451b7026b73e1b6cd44f85609538
Antoine Mariani <antoine.mariani@me.com>
2022-11-22 11:41:50 +0100

    first bookings routes & views

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,6 +1,6 @@
 class FlightsController < ApplicationController
-  # before_action :set_flight, only: %i[show edit update destroy]
-  
+  before_action :set_flight, only: %i[show edit update destroy]
+
   def index
     @flights = Flight.all
   end
--- db/migrate/20221121154822_create_bookings.rb
+++ db/migrate/20221121154822_create_bookings.rb
@@ -1,8 +1,8 @@
 class CreateBookings < ActiveRecord::Migration[7.0]
   def change
     create_table :bookings do |t|
-      t.references :users, null: false, foreign_key: true
-      t.references :flights, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+      t.references :flight, null: false, foreign_key: true
 
       t.timestamps
     end
