3def7c930dc134222e117abf353cd2b42e9c6d1f
antoinemariani <113173529+antoinemariani@users.noreply.github.com>
2022-11-22 11:44:30 +0100

    Revert "gem Pundit sans admin"

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,39 +2,30 @@ class FlightsController < ApplicationController
   before_action :set_flight, only: %i[show edit update destroy]
 
   def index
-    @flights = policy_scope(Flight)
     @flights = Flight.all
   end
 
-  def show
-    authorize @flight # Pundit
-  end
+  def show; end
 
   def new
     @flight = Flight.new
-    authorize @flight # Pundit
   end
 
   def create
     @flight = Flight.new(flight_params)
     @flight.user = current_user
-    authorize @flight # Pundit
     @flight.save!
     redirect_to flight_path(@flight)
   end
 
-  def edit
-    authorize @flight # Pundit
-  end
+  def edit; end
 
   def update
-    authorize @flight # Pundit
     @flight.update!(flight_params)
     redirect_to flight_path(@flight)
   end
 
   def destroy
-    authorize @flight # Pundit
     @flight.destroy!
     redirect_to flights_path, status: :see_other
   end
